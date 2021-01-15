ece3c06b783422f389facb5020543216158e7891
mengkki <guswnl0610@naver.com>
2021-01-15 21:53:00 +0900

    [modify] footer 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
+    ©<a href="https://github.com/guswnl0610">Mengkki</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
